bd77d4c0f398598281eefd390f307f24ad251c5c
Nicholas Abbott <abbottn24@yahoo.com>
2024-11-11 12:59:57 -0500

    update 3 11-11-24

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/*orginal code changed to below
-import express from 'express';
-import cors from 'cors';
-import register from './router/register.js';
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use('/register',register);        // localhost:5000/register
-app.get('/',(req, res) => {             // localhost:5000
-    res.send('howdy');
-});
-app.listen(5000, ()=> {
-    console.log('listening at port 5000');
-});*/
-
 import express from 'express';
 import cors from 'cors';
 import registerRouter from './router/register.js'; // Include .js extension
